627dfa68f7a9a8330592b5f0feedff3067885d3b
bo-iliev <boiliev44@gmail.com>
2021-02-24 15:37:54 +0000

    first commit

---
+++ resources/views/partials/graphs.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <iframe src="https://public.domo.com/cards/bWxVg" width="100%" height="600" marginheight="0" marginwidth="0"
+            frameborder="1"></iframe>
+
+    <iframe src="https://public.domo.com/cards/avnAV" width="100%" height="600" marginheight="0" marginwidth="0"
+            frameborder="1"></iframe>
+</div>
